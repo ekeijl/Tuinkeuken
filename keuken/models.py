4f23fb2ea5c2e0eead41b2c1e81894eec50cba4d
Mattijs Ugen <mattijsugen@gmail.com>
2012-12-15 21:05:38 +0100

    Povide simple Recipe model

--- keuken/models.py
+++ keuken/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.model):
+    author = models.ForeignKey(User)
+    # a free form text field containing the ingredient list
+    ingredients = models.TextField(blank = False)
+    # a free form text field containing the cooking directions
+    directions = models.TextField(blank = False)
