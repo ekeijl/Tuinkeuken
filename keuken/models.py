95765aac11252a33f3490ca47de4821d3e9f4801
Mattijs Ugen <mattijsugen@gmail.com>
2012-12-15 21:43:42 +0100

    Use recipe list from database, enable admin

--- keuken/models.py
+++ keuken/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Recipe(models.Model):
     author = models.ForeignKey(User)
+    pub_date = models.DateField(auto_now_add = True)
     # a free form text field containing the ingredient list
     ingredients = models.TextField(blank = False)
     # a free form text field containing the cooking directions
--- tuinkeuken/views.py
+++ tuinkeuken/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 
+from keuken.models import Recipe
+
 def index(request):
     return render_to_response('base.html', {
-        'recipes': ['bla']
+        'recipes': Recipe.objects.all().order_by('-pub_date')
     })
