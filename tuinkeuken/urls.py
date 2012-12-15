006e2aec0b28ee5c806d952a77c2ce6b6ecdce47
Mattijs Ugen <mattijsugen@gmail.com>
2012-12-15 21:00:46 +0100

    Implement hello world with index view

--- tuinkeuken/urls.py
+++ tuinkeuken/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tuinkeuken.views.home', name='home'),
+    url(r'^$', 'tuinkeuken.views.index', name='index'),
     # url(r'^tuinkeuken/', include('tuinkeuken.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
