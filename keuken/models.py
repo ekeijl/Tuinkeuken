16e2a2b57d776f584b77e62277d960c57a9c2794
Mattijs Ugen <mattijsugen@gmail.com>
2012-12-15 23:26:19 +0100

    Implement working recipe list and details page

--- keuken/models.py
+++ keuken/models.py
@@ -2,9 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Recipe(models.Model):
+    name = models.CharField(max_length = 256, blank = False)
     author = models.ForeignKey(User)
     pub_date = models.DateField(auto_now_add = True)
     # a free form text field containing the ingredient list
     ingredients = models.TextField(blank = False)
     # a free form text field containing the cooking directions
     directions = models.TextField(blank = False)
+
+    def __str__(self):
+        return self.name
--- keuken/views.py
+++ keuken/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import get_object_or_404, render_to_response
+
+from .models import Recipe
+
+def details(request, id):
+    return render_to_response('recipe-details.html', {
+        'recipe': get_object_or_404(Recipe, pk = id)
+    })
