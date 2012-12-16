bed0895c34b9f24c12366b0b860eb4344f9f2847
Mattijs Ugen <mattijsugen@gmail.com>
2012-12-16 14:47:08 +0100

    Move index page to index.html, use base.html as ... base

--- tuinkeuken/views.py
+++ tuinkeuken/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render_to_response
 from keuken.models import Recipe
 
 def index(request):
-    return render_to_response('base.html', {
+    return render_to_response('index.html', {
         'recipes': Recipe.objects.all().order_by('-pub_date')
     })
