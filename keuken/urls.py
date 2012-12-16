963cd821ae2ff9405c887a03bb9036f2f20c9851
Mattijs Ugen <mattijsugen@gmail.com>
2012-12-16 15:40:28 +0100

    Link to author page from details

--- keuken/urls.py
+++ keuken/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     # TODO: how is recipe-details made referable from template as keuken:recipe-details?
     url(r'(?P<id>\d+)$', 'keuken.views.details', name = 'recipe-details'),
+    url(r'(?P<author>\w+)$', 'keuken.views.list_by_author', name = 'recipes-by-author'),
 )
--- keuken/views.py
+++ keuken/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 
 from .models import Recipe
@@ -6,3 +8,6 @@ def details(request, id):
     return render_to_response('recipe-details.html', {
         'recipe': get_object_or_404(Recipe, pk = id)
     })
+
+def list_by_author(request, author):
+    return HttpResponse(get_object_or_404(User, username = author))
